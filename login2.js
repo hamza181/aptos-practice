e8cd822c4125db76d1c9ef78c3ef53f1f309e896
Ameer Hamza <ameerhamza@salsoft.net>
2025-05-12 15:09:51 +0500

    update

--- login2.js
+++ login2.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 import nacl from "tweetnacl";
 import naclUtil from "tweetnacl-util";
 const { encodeBase64 } = naclUtil;
-import { Account, Ed25519PrivateKey, PrivateKey } from "@aptos-labs/ts-sdk";
+import { Account, AccountAddress, Ed25519PrivateKey, PrivateKey } from "@aptos-labs/ts-sdk";
 import { HexString } from "aptos";
 // require("dotenv").config(
 //   { path: `.env.${env}` }
@@ -12,7 +12,8 @@ dotenv.config({
 });
 
 export const { PRIVATE_KEY } = process.env;
-
+const myAccountAddress =
+  "0xcb4c842efdde18072d22c03d34a886fed5c4e79e41ba8d31948900c04782b680";
 
 const makeId = (length) => {
   const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
@@ -23,21 +24,28 @@ const makeId = (length) => {
   return result;
 };
 
+const getAptosAccount = async () => {
+  const privateKey = new Ed25519PrivateKey(PRIVATE_KEY);
+  const address = AccountAddress.from(myAccountAddress);
+  if (!privateKey || !address) {
+    throw new Error("No private key or address found in local storage");
+  }
+  const account = Account.fromPrivateKey({ privateKey });
+  return account;
+};
+
 const privateKey = PRIVATE_KEY;
 const adminPrivateKey = Buffer.from(privateKey.slice(2), "hex");
 const keyPair = nacl.sign.keyPair.fromSeed(adminPrivateKey);
-
 const nonce = makeId(16);
-
 const message = "Sign Up Into Loonies" + nonce;
 
 const messageBuffer = Buffer.from(message, "utf-8");
-
 const signature = nacl.sign.detached(messageBuffer, keyPair.secretKey);
-
-const account = Account.fromPrivateKey({ privateKey });
+const account = await getAptosAccount();
 const signatureHex = HexString.fromBuffer(signature).toString();
+console.log('🚀 ~ signatureHex:', signatureHex);
 const publicKey = account.publicKey.toString();
 const wallet_address = account.accountAddress.toString();
-
-const encodedMessage = nacl.util.encodeBase64(messageBuffer);
+// const encodedMessage = nacl.util.encodeBase64(messageBuffer);
+const encodedMessage = encodeBase64(messageBuffer);
